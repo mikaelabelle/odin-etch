7aa655dd310e1e9d96c4ad9dc53a1edcc4693462
Mikaela Bell <19698567+mikaelabelle@users.noreply.github.com>
2024-03-19 12:01:49 +1000

    basic functionality

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-container = document.querySelector(".container")
+let container = document.querySelector(".container")
+let button = document.querySelector("#button")
+
+createGrid(16)
+squareFun()
+
+button.addEventListener("click", () => {
+    let gridDimensions = prompt("What size grid?")
+    createGrid(gridDimensions);
+    squareFun();
+})
 
 function createGrid(gridSize) {
     let totalNumGrids = gridSize * gridSize
+    container.innerHTML = ""
     for (let i = 1; i <= totalNumGrids; i++) {
         let tempDiv = document.createElement("div")
         tempDiv.classList.toggle('square')
-        tempDiv.style.flexBasis = `${(400 - (2 * gridSize)) / gridSize}px`
+        tempDiv.style.flexBasis = `${(400 - (gridSize)) / gridSize}px`
         tempDiv.style.minHeight = "1px"
         container.appendChild(tempDiv)
     }
 }
 
-createGrid(16);
+function squareFun() {
+    let squares = document.querySelectorAll(".square")
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.classList.add("squareSelected")
+        });
+    });
+
+}
+
+
+
+
